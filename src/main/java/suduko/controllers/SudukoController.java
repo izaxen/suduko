2b396f1ae96f4545244d00b4500d98c9070178de
Martin Isaksen <izaxen@telia.com>
2022-04-11 19:02:08 +0200

    Initial

--- src/main/java/suduko/controllers/SudukoController.java
+++ src/main/java/suduko/controllers/SudukoController.java
@@ -18,15 +18,15 @@ public class SudukoController {
     }
 
     public void sudukoControllers() {
-        app.get("/rest/getNewBoard", ctx -> {
+        app.get("api/getNewBoard", ctx -> {
             ctx.status(200).json(sudukoLogic.createNewBoard(Objects.requireNonNull(ctx.queryParam("level"))));
         });
 
-        app.get("/rest/getFullBoard", ctx -> {
+        app.get("api/getFullBoard", ctx -> {
             ctx.status(200).json(sudukoLogic.getFullSudokuBoard());
         });
 
-        app.post("/rest/validateInput", ctx -> {
+        app.post("api/validateInput", ctx -> {
             ctx.status(200).json(sudukoLogic.validateInputCells(ctx.body()));
 
         });
